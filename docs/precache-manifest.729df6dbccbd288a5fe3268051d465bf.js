75e8bb84a63b495a8fcfe0b46b409d4d368b25d2
Danai.T <danai.thit@gmail.com>
2018-10-10 16:28:38 +0700

    commit build

---
+++ docs/precache-manifest.729df6dbccbd288a5fe3268051d465bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bfc5fc2a8d11efc1ed38",
+    "url": "/static/js/main.bfc5fc2a.chunk.js"
+  },
+  {
+    "revision": "05e7fd68e8da08f14d4e",
+    "url": "/static/js/1.05e7fd68.chunk.js"
+  },
+  {
+    "revision": "bfc5fc2a8d11efc1ed38",
+    "url": "/static/css/main.c9c9fc33.chunk.css"
+  },
+  {
+    "revision": "514afa80dcebbd8f57554f008229572d",
+    "url": "/index.html"
+  }
+];
